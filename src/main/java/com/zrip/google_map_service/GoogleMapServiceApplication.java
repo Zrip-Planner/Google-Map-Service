00a6fcdd841149f917dca802f37de9cb38f8b1a2
Mohd Faizan <zanhd982@gmail.com>
2024-07-27 18:56:50 +0530

    [setup] initail-spring-boot-setup

---
+++ src/main/java/com/zrip/google_map_service/GoogleMapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zrip.google_map_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zrip/google_map_service/GoogleMapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrip.google_map_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleMapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
